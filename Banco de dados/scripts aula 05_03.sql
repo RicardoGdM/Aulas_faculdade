bc763a876675fbbe564e5adabcc64d25d4f06f71
RicardoGdM <rickgdm2016@gmail.com>
2025-03-26 20:44:01 -0300

    26/03

--- Banco de dados/scripts aula 05_03.sql
+++ Banco de dados/scripts aula 05_03.sql
@@ -1,11 +1,27 @@
 insert into pessoas (
 	tipo, nome, status, email, data_nasc, fornecedor, transportadora, representante, funcionario, parceiro_comercial
 	)
-	values (
+values (
 	2, 'Ricardo Gomes de Moraes', 1, 'teste@teste.com', '1995-09-09', 0, 0, 0, 0, 0
 	);
 
+insert into pessoas (
+	tipo, nome, status, email, data_nasc, fornecedor, transportadora, representante, funcionario, parceiro_comercial
+	)
+values (
+	1, 'João Antônio', 1, 'joao@teste.com', '1985-09-09', 1, 0, 0, 0, 0
+	);
+
+insert into pessoas (
+	tipo, nome, status, email, data_nasc, fornecedor, transportadora, representante, funcionario, parceiro_comercial
+	)
+	values (
+	1, 'Maria Cristina', 1, 'maria@teste.com', '2005-09-09', 0, 0, 0, 1, 0
+	);
+
 select * from pessoas;
+
+select * from pessoas where fornecedor = 1;
 
 update pessoas set 
 	tipo = 3, 
@@ -20,10 +36,28 @@ insert into endereco (
 	1, 1, 'Brasil', 'SP', 'São Paulo', 'Americana', 'Centro', 'Rua principal','100', 'Sala 2', 'Ao lado de lá'
 );
 
+insert into endereco (
+	fk_pessoas, tipo, pais, uf, estado, cidade, bairro, rua, numero, complemento, obs
+) values (
+	2, 1, 'Brasil', 'RJ', 'Rio de Janeiro', 'Rio de Janeiro', 'Centro', 'Rua principal','1000', 'Sala 20', 'Ao lado de cá'
+);
+
+insert into endereco (
+	fk_pessoas, tipo, pais, uf, estado, cidade, bairro, rua, numero, complemento, obs
+) values (
+	3, 1, 'Brasil', 'SP', 'São Paulo', 'Santa Bárbara d Oeste', 'Centro', 'Rua principal','200', 'Sala 2', 'Ao lado de lá'
+);
+
 select * from endereco;
 
 insert into contato (
 	fk_pessoas, email, telefone
 ) values (
 	1, 'teste2@teste.com', '(19)99302-9444'
+);
+
+insert into contato (
+	fk_pessoas, email, telefone
+) values (
+	3, 'joao@teste.com', '(19)99302-9555'
 );
--- Banco de dados/scripts aula 19_03.sql
+++ Banco de dados/scripts aula 19_03.sql
@@ -30,11 +30,26 @@ insert into produtos(
 	'Padaria', 'P-001', '2.99', '2.99', '0', '0', 29.99
 );
 
+insert into produtos(
+	fk_fornecedor, nome, imagem, descricao, marca, categoria, referencia, preco_fornecedor,
+	custo, markup_minimo, markup_venda, preco_venda
+) values (
+	1, 'Café', 'https://m.media-amazon.com/images/I/61cjsci53VL._AC_SL1000_.jpg', 'Café torrado', 'Cafezin',
+	'Cafeteria', 'C-001', '2.99', '2.99', '0', '0', 49.99
+);
+
 insert into estoque(
 	fk_produto, cod_barras, unidade_medida, cor, tamanho, lote, validade,
 	estoque_minimo, estoque_quantidade, ativo
 ) values(
 	1, '00000001', 'UN', 'Padrão', 'M', '2025-03-19-A', '2025-03-22', '3', '8', 1
+);
+
+insert into estoque(
+	fk_produto, cod_barras, unidade_medida, cor, tamanho, lote, validade,
+	estoque_minimo, estoque_quantidade, ativo
+) values(
+	2, '00048523', 'UN', 'Padrão', 'M', '2025-01-19-A', '2025-07-22', '3', '15', 1
 );
 
 insert into pedidos (
@@ -61,10 +76,65 @@ select * from pedido_item;
 select 
 	-- Pedido
 	p.status AS status_pedido,
+	p.id AS cod_pedido,
 
 	-- Pessoa
-	pe.nome as nome_pessoa,
+	pe.nome as nome_pessoa
 
 from pedidos p 
 left join pessoas pe on p.fk_pessoas = pe.id
 left join pedido_item pi on pi.fk_pedidos = p.id;
+
+/*LEFT JOIN exibe todos os resultados, priorizando a tabela da esquerda*/
+SELECT 
+	-- pessoas
+	p.id AS pessoa_id,
+	p.nome,
+	p.email,
+
+	-- enderecos
+	e.id AS pessoa_id,
+	e.uf,
+	e.cidade,
+
+	-- contato
+	c.telefone
+
+FROM pessoas p
+LEFT JOIN endereco e ON p.id = e.fk_pessoas
+LEFT JOIN contato c ON p.id = c.fk_pessoas;
+
+/* RIGHT JOIN trás somente os dados que estão completos, dando prioridade a tabela da direita*/
+SELECT 
+	-- pessoas
+	p.id AS pessoa_id,
+	p.nome,
+	p.email,
+
+	-- enderecos
+	e.id AS pessoa_id,
+	e.uf,
+	e.cidade
+FROM pessoas p
+RIGHT JOIN endereco e ON p.id = e.fk_pessoas;
+
+SELECT 
+	-- produtos
+	p.id AS produto_id,
+	p.nome,
+	p.imagem,
+	p.descricao,
+
+	-- fornecedor
+	pes.id AS id_fornecedor,
+	pes.nome AS nome_fornecedor,
+	pes.email,
+	-- ESTOQUE
+	e.cod_barras,
+	e.unidade_medida,
+	e.cor,
+	e.validade
+FROM produtos p
+LEFT JOIN estoque e ON p.id = e.fk_produto
+LEFT JOIN pessoas pes ON p.fk_fornecedor = pes.id
+WHERE e.ativo = 1;
