3be6d1b293431c855fde1b550a7f4db6b76858ed
RicardoGdM <rickgdm2016@gmail.com>
2025-04-23 21:30:29 -0300

    23-04

---
+++ Banco de dados/scripts aula 23_04_2.sql
@@ -0,0 +1,9 @@
+SELECT * FROM sys.tables;
+
+SELECT * FROM invoices;
+
+SELECT COUNT(CustomerID) FROM customers;
+
+SELECT TOP 1 * FROM customers ORDER BY CustomerID DESC;
+
+SELECT * FROM Employees;
